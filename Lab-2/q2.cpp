9180a5c1fc8c335705d883abe3661bf49ee9ea05
Sandip Sapkota <079bct076@ioepc.edu.np>
2024-06-10 19:55:11 +0545

    chore: Refactor q2.cpp for improved readability and modularity

--- Lab-2/q2.cpp
+++ Lab-2/q2.cpp
@@ -1,18 +1,33 @@
 // Write a program using the function overloading that converts feet to inches. Use function with no argument, one argument and two arguments. Decide yourself the types of arguments. Use pass by reference in any one of the function above.
-
 #include <iostream>
 using namespace std;
-
-void convert(int &feet, int &inches)
-{
-    inches = feet * 12;
-}
+float conversion();
+float conversion(float);
+void conversion(float, float &);
 int main()
 {
-    int feet, inches;
-    cout << "Enter the feet: ";
-    cin >> feet;
-    convert(feet, inches);
-    cout << "The inches are: " << inches << endl;
-    return 0;
+    float ft;
+    cout << "Enter number in feet:\t" << endl;
+    cin >> ft;
+    float ans;
+    float &a = ans;
+    cout << "answer from function with no parameter:\t" << conversion() << endl;
+    cout << "answer from function with single parameter:\t" << conversion(ft) << endl;
+    conversion(ft, a);
+    cout << "answer from function with two parameter:\t" << ans << endl;
+}
+void conversion(float h, float &b)
+{
+    b = h * 12;
+}
+float conversion(float h)
+{
+    return h * 12;
+}
+float conversion()
+{
+    float temp;
+    cout << "Enter value of feet for function with no argument:\t" << endl;
+    cin >> temp;
+    return temp * 12;
 }
