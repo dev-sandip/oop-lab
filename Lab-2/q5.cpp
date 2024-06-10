2feb5ffb74578ba838d52d0c09dbfc3b414b2b20
Sandip Sapkota <079bct076@ioepc.edu.np>
2024-06-10 19:59:50 +0545

    chore: Update q5.cpp to calculate net payment for employee

--- Lab-2/q5.cpp
+++ Lab-2/q5.cpp
@@ -1,16 +1,15 @@
 // Assume that employee will have to pay 10 percent income tax to the government. Ask user to enter the employee salary. Use inline function to display the net payment to the employee by the company.
 #include <iostream>
 using namespace std;
-inline void net_payment(float salary)
-{
-    float tax = 0.1 * salary;
-    cout << "Net payment to the employee by the company is: " << salary - tax << endl;
-}
+float net(int);
 int main()
 {
-    float salary;
-    cout << "Enter the salary of the employee: ";
-    cin >> salary;
-    net_payment(salary);
-    return 0;
+    int s;
+    cout << "Enter your salary:\t" << endl;
+    cin >> s;
+    cout << "Net payment to the employee = " << net(s);
+}
+inline float net(int a)
+{
+    return (a - (a * 0.1));
 }
